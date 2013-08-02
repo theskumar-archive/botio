c2eb6067466886dbc867b099e17dce8cbff609f7
Saurabh Kumar <thes.kumar@gmail.com>
2013-08-03 00:34:00 +0530

    Add fabric file for managing deploy and dev envirnment

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 NATHAN
 =======
 
-
 This is a version of GitHub's Campfire bot, hubot. He's pretty cool.
 
+
+## Running local Testing server
+
+Use `fab devserver`
 
 ----
 Further updates soon
---
+++ fabfile.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import os
+from fabric.api import local
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+def devserver():
+    print (cwd)
+    local(os.path.join(cwd, 'bin/hubot_local'))
+
+def deploy():
+    local('git push heroku master')
+    local('heroku logs')
